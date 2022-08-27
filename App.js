543f74372789d346514973c13ce62cbaacfac655
Mohankrishna1998 <mohankrishnagajula19@gmail.com>
2022-08-27 20:53:19 +0530

    first commit

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { setStatusBarBackgroundColor, StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import InShortTabs from './components/InShortTabs';
+import Context from './API/Context';
 
-export default function App() {
+ function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{...styles.container, backgroundColor: "#282C35"}}>
+      <InShortTabs />
     </View>
   );
 }
@@ -13,8 +15,14 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginTop: StatusBar.currentHeight,
   },
 });
+
+export default () => {
+  return (
+    <Context>
+    <App/>
+  </Context>
+  )
+}
